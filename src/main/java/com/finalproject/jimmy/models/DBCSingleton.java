a982527436451dfcbc2052e4f0aba5d18515c7d1
Jimmy Karlsson <47915308+preaker81@users.noreply.github.com>
2023-06-01 14:02:29 +0200

    Cleaned up DBCSingleton.java and added helpful comments

--- src/main/java/com/finalproject/jimmy/models/DBCSingleton.java
+++ src/main/java/com/finalproject/jimmy/models/DBCSingleton.java
@@ -10,68 +10,83 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class DBCSingleton {
-    private static volatile DBCSingleton db;
+    private static volatile DBCSingleton db; // Singleton instance
     private Connection connection;
     private MysqlDataSource dataSource;
-
     private static final Logger logger = LogManager.getLogger(DBCSingleton.class);
 
+    // Private constructor to prevent instantiation
     private DBCSingleton() {
-        loadDatabaseProperties();
-        createConnection();
-    }
-
-    private void loadDatabaseProperties() {
-        Properties props = new Properties();
-
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("database.properties")) {
-            if (input == null) {
-                logger.error("Unable to find database.properties");
-                return;
-            }
-
-            // load properties file
-            props.load(input);
-
-            String url = props.getProperty("hostname");
-            int port = Integer.parseInt(props.getProperty("port"));
-            String database = props.getProperty("dbname");
-            String username = props.getProperty("username");
-            String password = props.getProperty("password");
-
-            dataSource = new MysqlDataSource();
-            dataSource.setUser(username);
-            dataSource.setPassword(password);
-            dataSource.setUrl("jdbc:mysql://" + url + ":" + port + "/" + database + "?serverTimezone=UTC");
-            dataSource.setUseSSL(false);
-
-        } catch (Exception e) {
-            logger.error("Error loading database properties", e);
-        }
-    }
-
-    private void createConnection() {
         try {
-            connection = dataSource.getConnection();
-        } catch (SQLException e) {
-            logger.error("Error creating connection", e);
+            // Load database properties from a properties file
+            loadDatabaseProperties();
+            // Establish a connection
+            createConnection();
+        } catch (Exception e) {
+            logger.error("Error during database initialization", e);
         }
+    }
+
+    private void loadDatabaseProperties() throws Exception {
+        Properties props = new Properties();
+        // Load the properties file
+        InputStream input = getClass().getClassLoader().getResourceAsStream("database.properties");
+
+        if (input == null) {
+            throw new Exception("Unable to find database.properties");
+        }
+
+        // Load properties from the properties file
+        props.load(input);
+
+        // Get properties
+        String url = props.getProperty("hostname");
+        int port = Integer.parseInt(props.getProperty("port"));
+        String database = props.getProperty("dbname");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        // Set database connection properties
+        dataSource = new MysqlDataSource();
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl("jdbc:mysql://" + url + ":" + port + "/" + database + "?serverTimezone=UTC");
+        dataSource.setUseSSL(false);
+    }
+
+    private void createConnection() throws SQLException {
+        // Only create a new connection if the current one doesn't exist or is closed
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+        // Create a new connection
+        connection = dataSource.getConnection();
     }
 
     public static Connection getConnection() {
-        try {
+        // The 'double-checked locking' pattern is used here to ensure thread-safety
+        DBCSingleton result = db;
+        if (result == null) {
             synchronized (DBCSingleton.class) {
-                if (db == null) {
+                result = db;
+                if (result == null) {
                     db = new DBCSingleton();
-                } else if (db.connection == null || db.connection.isClosed()) {
-                    db.createConnection();
+                    result = db;
                 }
             }
-        } catch (SQLException e) {
-            logger.error("Error checking or creating connection", e);
         }
-
-        return db.connection;
+        // Return the connection object
+        return result.connection;
     }
 
+    public static void close() {
+        // Close the connection if it exists and is open
+        if (db != null && db.connection != null) {
+            try {
+                db.connection.close();
+            } catch (SQLException e) {
+                logger.error("Error closing connection", e);
+            }
+        }
+    }
 }
